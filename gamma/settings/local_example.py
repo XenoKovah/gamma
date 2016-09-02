a8bb3498bf6eaa99d06ce87a12fb20ad3cf5e2ea
Maksim Sokolskiy <misokolsky@gmail.com>
2016-09-02 13:04:08 +0300

    chore(settings): add MySQL settings to `local_example.py`

--- gamma/settings/local_example.py
+++ gamma/settings/local_example.py
@@ -8,3 +8,14 @@ SOCIAL_AUTH_EDX_OIDC_URL_ROOT = 'https://lms-domain-name/oauth2'
 
 # This value should be the same as SOCIAL_AUTH_EDX_OIDC_SECRET
 SOCIAL_AUTH_EDX_OIDC_ID_TOKEN_DECRYPTION_KEY = SOCIAL_AUTH_EDX_OIDC_SECRET
+
+
+# For MySQL DB
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/path/to/my.cnf',
+        },
+    }
+}
