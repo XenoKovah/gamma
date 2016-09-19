d0816237f6e76b9e7024beda5220430a413cb4a6
Maksim Sokolskiy <misokolsky@gmail.com>
2016-09-19 13:29:40 +0300

    feat(dashboard): add user rank, add progress data

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .utils import MongoConnector
+from .models import GameProfile
 
 
 class DashboardView(View):
@@ -12,11 +13,21 @@ class DashboardView(View):
     conn = MongoConnector()
 
     def get(self, request):
-        user_achievements = None
-        progress_data = self.conn.get_progress(request.user)
+        user_achievements, rank, progress_data = None, None, None
         if request.user.is_authenticated():
+            progress_data = self.conn.get_progress(request.user)
             user_achievements = request.user.userachievement_set.select_related('achievement').all()
+            top = [
+                _id
+                for i in GameProfile.objects.order_by('-points')[:100].values_list('id')
+                for _id in i
+            ]
+            try:
+                rank = top.index(request.user.gameprofile.id) + 1
+            except ValueError:
+                pass
         return render(request, 'dashboard.html', {
             'progress_data': progress_data,
+            'rank': rank,
             'user_achievements': user_achievements
         })
