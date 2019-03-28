c344065c152d1fd6dec92d72fa9447407944e1df
ihor-romaniuk <Igorek25>
2019-03-28 19:01:04 +0200

    [Gamma] fixed charts, badges popup

--- frontend/admin-panel/src/components/LeaderBoardModal.js
+++ frontend/admin-panel/src/components/LeaderBoardModal.js
@@ -7,9 +7,10 @@ export default class LeaderBoardModal extends React.Component {
     }
 
     render() {
+      let wrapperClass = this.props.assistentClass ? 'LeaderboardModalWrapper LeaderboardModalWrapper_collection' : 'LeaderboardModalWrapper';
         return (
             <div className="LeaderboardModal">
-                <div className="LeaderboardModalWrapper">
+                <div className={wrapperClass}>
                     <button className="LeaderboardModal-Close" onClick={this.handleClose.bind(this)}></button>
                     {this.props.children}
                 </div>
