35d931085c1e0dbcd3a642216ddecd633344eb9f
RuslanSkiraRaccoongang <ruslan.skira@raccoongang.com>
2019-08-12 16:45:36 +0300

    Show progress pop up on all badges RGG-39
    Changes:
    - Add badge name to the badge progress popup

--- frontend/admin-panel/src/containers/MoreBadges.js
+++ frontend/admin-panel/src/containers/MoreBadges.js
@@ -31,7 +31,7 @@ export default class MoreBadges extends React.Component {
             <LeaderBoardModal history={this.props.history} assistentClass={true} >
                 {
                     Object.keys(this.state.badges).map((badge, ind) => {
-                        let badgeItemClass = !this.state.badges[badge].done ? 
+                        let badgeItemClass = !this.state.badges[badge].done ?
                             'BadgeItemFigure BadgeItemFigure_disable' : 'BadgeItemFigure';
                         return (
                             <div className="BadgeItem BadgeItem_center" key={ind}>
@@ -39,6 +39,24 @@ export default class MoreBadges extends React.Component {
                                     <img src={process.env.REACT_APP_LOCALHOST + this.state.badges[badge].url} alt="" className="BadgeItemFigure-Image" />
                                 </div>
                                 <div className="BadgeItem-Name">{badge}</div>
+                                <div className="BadgeItemPopup">
+                                    <div className="BadgeItemPopup-Head">{badge}</div>
+                                    <div className="BadgeItemPopup-Body">
+                                    <ul className="BadgeItemPopupList">
+                                        {
+                                            Object.keys(this.state.badges[badge]['progress']).map((p)=>{
+                                                return (
+                                                    <li className="BadgeItemPopupList-Item">
+                                                        <span className="BadgeItemPopupList-Counter">
+                                                            {this.state.badges[badge]['progress'][p]['count']} /  { this.state.badges[badge]['progress'][p]['goal']} {p[0].toUpperCase() + p.slice(1).toLowerCase()}
+                                                        </span>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                    </div>
+                                </div>
                             </div>
                         )
                     })
