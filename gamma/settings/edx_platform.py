e6d4f96231e4301c6734b9f73672acfd63cf3776
opersian_rg <olena.persianova@raccoongang.com>
2019-12-04 11:55:43 +0200

    RGG-66/71: Change EDX_API_V2_SUFFIX

--- gamma/settings/edx_platform.py
+++ gamma/settings/edx_platform.py
@@ -3,4 +3,4 @@ Settings related to integration with edX platform.
 """
 
 EDX_API_VERSION = "v2"
-EDX_API_V2_SUFFIX = f"/api/courses/{EDX_API_VERSION}/"
+EDX_API_V2_SUFFIX = f"/api/courses/{EDX_API_VERSION}/gamma/"
