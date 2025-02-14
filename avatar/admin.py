fbc68115c5dd713c5db6c49ba8c7a4b7f3d02b9b
Kyrylo Kireiev <kyrylo.kireiev@raccoongang.com>
2025-02-14 11:17:22 +0000

    refactor: [NAU-253] Refactor Avatar models

--- avatar/admin.py
+++ avatar/admin.py
@@ -1,50 +1,23 @@
 from django.contrib import admin
 
-from avatar.models import (
-    AvatarBase,
-    AvatarColor,
-    AvatarItem,
-    AvatarSet,
-    AvatarSetItem,
-    SkinType,
-    UserAvatarConfig,
-)
+from avatar.models import Avatar, AvatarSet, UserAvatarConfig
 
 
-@admin.register(UserAvatarConfig)
-class UserAvatarConfigAdmin(admin.ModelAdmin):
-    list_display = ('user', 'image', 'avatar_set', 'use_avatar')
-    list_filter = ('use_avatar', 'avatar_set')
+@admin.register(Avatar)
+class AvatarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title',)
+    filter_horizontal = ('rules',)
 
 
 @admin.register(AvatarSet)
 class AvatarSetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'base_image', 'avatar_color')
-    filter_horizontal = ('avatar_items',)
+    list_display = ('title', 'is_draft')
+    search_fields = ('title',)
+    filter_horizontal = ('avatar',)
 
 
-@admin.register(AvatarItem)
-class AvatarItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'skin_type')
-    list_filter = ('skin_type',)
-
-
-@admin.register(SkinType)
-class SkinTypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
-@admin.register(AvatarColor)
-class AvatarColorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'hex_color')
-
-
-@admin.register(AvatarBase)
-class AvatarBaseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'base_image')
-
-
-@admin.register(AvatarSetItem)
-class AvatarSetItemAdmin(admin.ModelAdmin):
-    list_display = ('avatar_base', 'avatar_item')
-    list_filter = ('avatar_base', )
+@admin.register(UserAvatarConfig)
+class UserAvatarConfigAdmin(admin.ModelAdmin):
+    list_display = ('user', 'selected_avatar', 'avatar_set')
+    search_fields = ('user__username',)
--- avatar/models.py
+++ avatar/models.py
@@ -1,81 +1,61 @@
 from django.db import models
-from django.core.validators import FileExtensionValidator, RegexValidator
+from django.core.validators import FileExtensionValidator
 from django.utils.translation import ugettext_lazy as _
 
-from avatar.constants import HEX_COLOR_REGEX
 from users.models import GammaUser
 
 
-class SkinType(models.Model):
+class Avatar(models.Model):
     """
-    Model representing different skin types for Avatar items (e.g., Glasses, Headdress, Outerwear).
-    """
-
-    name = models.CharField(max_length=255, verbose_name=_('Skin Type'))
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = _('Skin Type')
-        verbose_name_plural = _('Skin Types')
-
-
-class AvatarColor(models.Model):
-    """
-    Model representing Avatar colors.
+    Model representing the Gamma User Avatar.
     """
 
-    name = models.CharField(max_length=255, verbose_name=_('Color Name'))
-    hex_color = models.CharField(
-        max_length=7,
-        verbose_name=_('Hex Color Code'),
-        validators=[RegexValidator(HEX_COLOR_REGEX, _('Enter a valid hex color code'))]
-    )
-
-    def __str__(self):
-        return f'{self.name} ({self.hex_color})'
-
-    class Meta:
-        verbose_name = _('Avatar Color')
-        verbose_name_plural = _('Avatar Colors')
-
-
-class AvatarBase(models.Model):
-    """
-    Model representing the base image for the User Avatar creation.
-    """
-
-    name = models.CharField(max_length=255, verbose_name=_('Base Name'))
-    base_image = models.FileField(
-        upload_to='static/images/avatar_bases/',
+    title = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('Avatar Title'))
+    description = models.TextField(null=True, blank=True, verbose_name=_('Avatar Description'))
+    image = models.FileField(
+        upload_to='uploads/avatars/',
         validators=[FileExtensionValidator(allowed_extensions=['svg'])],
-        null=True,
-        blank=True
     )
+    rules = models.ManyToManyField('rules.Rule', blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
 
     class Meta:
-        verbose_name = _('Avatar Base')
-        verbose_name_plural = _('Avatar Bases')
+        verbose_name = _('Avatar')
+        verbose_name_plural = _('Avatars')
+
+
+class AvatarSet(models.Model):
+    """
+    Model representing a set of Avatars and its attributes.
+
+    A type of evolution where each new object is unlocked based on
+    the user's progress, achievements,
+    or intermediate results related to the course.
+    """
+
+    title = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('Avatar Set Title'))
+    avatar = models.ManyToManyField(Avatar, blank=True, verbose_name=_('Avatar'))
+    use_in_courses = models.JSONField(default=list)
+    is_draft = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Avatar Set')
+        verbose_name_plural = _('Avatar Sets')
 
 
 class UserAvatarConfig(models.Model):
     """
-    Top-level model representing the User's Avatar configuration.
+    Model representing the Gamma User's Avatar configuration.
     """
 
-    user = models.OneToOneField(GammaUser, on_delete=models.CASCADE)
-    image = models.FileField(
-        upload_to='static/images/user_avatars/',
-        validators=[FileExtensionValidator(allowed_extensions=['svg', 'png', 'jpg', 'jpeg', 'heic'])],
-        null=True,
-        blank=True
-    )
-    avatar_set = models.ForeignKey('AvatarSet', on_delete=models.SET_NULL, null=True)
-    use_avatar = models.BooleanField(default=False, verbose_name=_('Use this avatar?'))
+    user = models.ForeignKey(GammaUser, on_delete=models.CASCADE)
+    selected_avatar = models.ForeignKey(Avatar, null=True, blank=True, on_delete=models.CASCADE)
+    avatar_set = models.ForeignKey(AvatarSet, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.username}'s Avatar Config"
@@ -83,59 +63,3 @@ class UserAvatarConfig(models.Model):
     class Meta:
         verbose_name = _('User Avatar Config')
         verbose_name_plural = _('User Avatar Configs')
-
-
-class AvatarSet(models.Model):
-    """
-    Model representing a set of avatar items and attributes.
-    """
-    name = models.CharField(max_length=255, verbose_name=_('Set Name'))
-    base_image = models.ForeignKey(AvatarBase, on_delete=models.SET_NULL, null=True, verbose_name=_('Base Image'))
-    avatar_items = models.ManyToManyField('AvatarSetItem', blank=True, verbose_name=_('Avatar Items'))
-    avatar_color = models.ForeignKey(AvatarColor, on_delete=models.SET_NULL, null=True, verbose_name=_('Avatar Color'))
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = _('Avatar Set')
-        verbose_name_plural = _('Avatar Sets')
-
-
-class AvatarItem(models.Model):
-    """
-    Model representing individual avatar items (e.g., Glasses, Headdress, Outerwear).
-    """
-
-    name = models.CharField(max_length=255, verbose_name=_('Name'))
-    skin_type = models.ForeignKey(SkinType, on_delete=models.CASCADE)
-    svg_file = models.FileField(
-        upload_to='static/images/avatar_items/',
-        validators=[FileExtensionValidator(allowed_extensions=['svg'])],
-        null=True,
-        blank=True
-    )
-
-    def __str__(self):
-        return f'{self.name} ({self.skin_type.name})'
-
-    class Meta:
-        verbose_name = _('Avatar Item')
-        verbose_name_plural = _('Avatar Items')
-
-
-class AvatarSetItem(models.Model):
-    """
-    Model representing the Avatar Item specified for definitive Avatar Set.
-    """
-
-    avatar_base = models.ForeignKey(AvatarBase, on_delete=models.CASCADE)
-    avatar_item = models.ForeignKey(AvatarItem, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.avatar_item} for {self.avatar_base}'
-
-    class Meta:
-        verbose_name = _('Avatar Set Item')
-        verbose_name_plural = _('Avatar Set Items')
-        unique_together = (('avatar_base', 'avatar_item'),)
